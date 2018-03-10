f44185fc27e452c713a78093e399bbfa248bf31a
zhi <zzqlrr@gmail.com>
2018-03-10 18:08:43 +0800

    一些例子

--- src/cb1_1.py
+++ src/cb1_1.py
@@ -58,8 +58,34 @@ q.appendleft(6)
 q.pop()
 print(q, "-" * 10, end="\n")
 
-
-# 找出集合中最大最小的元素
+# 找出集合中最大最小的元素,heapq模块
 import heapq
-nums=[1,2,3,4,5,6,4,3,8,43,0]
-heapq.nlargest()
+
+nums = [1, 2, 3, 4, 5, 6, 4, 3, 8, 43, 0]
+print(heapq.nlargest(3, nums))
+print(heapq.nsmallest(3, nums))
+# 接受一个key，自定义排序
+profile = [{"name": "zhi", "price": 101}, {"name": "lrr", "price": 99}]
+print(heapq.nlargest(1, profile, key=lambda s: s["price"]))
+
+# 创建一键多值变量 defaultdic
+from _collections import defaultdict
+
+tempd = defaultdict(list)
+tempd['a'].append(1)
+tempd['a'].append(2)
+tempd['b'].append(3)
+for key in tempd:
+    print(tempd[key])
+
+# 让字典保持有序，OrderedDict，内部维护了一个双向链表，大小是普通字典的2倍多
+# 应用场景实例，在进行Json编码时精确控制各字段的顺序，那么首先在OrderedDict中构建数据就可以了
+from _collections import OrderedDict
+
+od = OrderedDict()
+od['foo'] = 1
+od['ds'] = 2
+od['rt'] = 3
+for key in od:
+    print(od[key])
+zip()
